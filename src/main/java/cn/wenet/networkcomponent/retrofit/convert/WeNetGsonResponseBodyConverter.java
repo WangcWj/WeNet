301466aae94933a95708f483ea2fcc063ac5b2d2
WangChaoChao <wangchaochaoj@163.com>
2020-11-04 19:13:03 +0800

    1.对请求结果进行预处理：格式校验，编码校验等。

--- src/main/java/cn/wenet/networkcomponent/retrofit/convert/WeNetGsonResponseBodyConverter.java
+++ src/main/java/cn/wenet/networkcomponent/retrofit/convert/WeNetGsonResponseBodyConverter.java
@@ -3,13 +3,18 @@ package cn.wenet.networkcomponent.retrofit.convert;
 import com.google.gson.Gson;
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
+
 import org.json.JSONObject;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.nio.charset.Charset;
+import java.util.List;
 
 import cn.wenet.networkcomponent.base.NetBaseResultBean;
 import okhttp3.MediaType;
@@ -17,17 +22,19 @@ import okhttp3.ResponseBody;
 import retrofit2.Converter;
 
 /**
- *
+ * 对接口请求进行预处理。
  */
 public class WeNetGsonResponseBodyConverter<T> implements Converter<ResponseBody, T> {
 
     private final Gson gson;
     private final TypeAdapter<T> adapter;
+    private Type mReturnType;
     private static final Charset UTF_8 = Charset.forName("UTF-8");
 
-    WeNetGsonResponseBodyConverter(Gson gson, TypeAdapter<T> adapter) {
+    WeNetGsonResponseBodyConverter(Gson gson, Type returnType, TypeAdapter<T> adapter) {
         this.gson = gson;
         this.adapter = adapter;
+        this.mReturnType = returnType;
     }
 
     @Override
@@ -55,5 +62,39 @@ public class WeNetGsonResponseBodyConverter<T> implements Converter<ResponseBody
             value.close();
         }
         return null;
+    }
+
+    private boolean checkResult(JSONObject jsonObject) {
+        try {
+            //只检查参数类型的，比如BaseRequest<String>。
+            if (mReturnType instanceof ParameterizedType) {
+                ParameterizedType pt = (ParameterizedType) mReturnType;
+                Type actualTypeArgument = pt.getActualTypeArguments()[0];
+                if (actualTypeArgument instanceof Class) {
+                    return handlerResult((Class<?>) actualTypeArgument, jsonObject);
+                } else if (actualTypeArgument instanceof ParameterizedType) {
+                    ParameterizedType innerPt = (ParameterizedType) actualTypeArgument;
+                    Class<?> rawType = (Class<?>) innerPt.getRawType();
+                    return handlerResult(rawType, jsonObject);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    private boolean handlerResult(Class<?> clz, JSONObject jsonObject) throws Exception {
+        String data = String.valueOf(jsonObject.get("data"));
+        if (clz == List.class) {
+            //BaseRequest<T> T为List。
+            return data.startsWith("[") && data.endsWith("]");
+        } else if (clz == String.class) {
+            //do something
+        } else {
+            //BaseRequest<T> T为Object。
+            return data.startsWith("{") && data.endsWith("}");
+        }
+        return true;
     }
 }
